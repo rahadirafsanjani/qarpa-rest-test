7e43f13a327c9ccfd68fd21016c58498e6a2c5cd
rahadi rafsanjani <rahadirafsanjani@gmail.com>
2023-12-04 06:20:48 +0700

    [Leave] Create data generator and crete post and put endpoint hitter

---
+++ Leave/Resources/generator.py
@@ -0,0 +1,53 @@
+from faker import Faker
+import random
+import string
+
+class generator:
+    def __init__(self):
+        self.fake = Faker("id_ID")
+
+    def date_future(self):
+        return  self.fake.date_between(start_date='today', end_date='+60d').strftime('%Y-%m-%d')
+    def date_past(self):
+        return  self.fake.date_between(start_date='-60d', end_date='today').strftime('%Y-%m-%d')
+
+    def default_sentence(self):
+        return  self.fake.text(max_nb_chars=40)
+
+    def make_sentence(self, length, digits=False, punctuation=False, whitespace=False):
+        if length < 0:
+            return "Length must be a non-negative integer."
+
+        characters = string.ascii_letters
+        if digits:
+            characters += string.digits
+        if punctuation:
+            characters += string.punctuation
+        if whitespace:
+            characters += string.whitespace
+
+        if not characters:
+            return "No valid character set selected."
+
+        # Generate a random sentence of the specified length
+        sentence = ''.join(random.choice(characters) for _ in range(length))
+
+        return sentence
+    
+        
+    def make_sentence_only_white_space(self, length, whitespace=True):
+        if length < 0:
+            return "Length must be a non-negative integer."
+
+        characters = ""
+        if whitespace:
+            characters += string.whitespace
+
+        if not characters:
+            return "No valid character set selected."
+
+        # Generate a random sentence of the specified length
+        sentence = ''.join(random.choice(characters) for _ in range(length))
+
+        return sentence
+    
