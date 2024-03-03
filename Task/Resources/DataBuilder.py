719fab06ad66771cbe9ca4a3d937c9723454a18b
rahadi rafsanjani <rahadirafsanjani@gmail.com>
2024-03-03 23:14:27 +0700

    [Task] Rebuild Data Generator

--- Task/Resources/DataBuilder.py
+++ Task/Resources/DataBuilder.py
@@ -1,71 +1,98 @@
 from faker import Faker
-from datetime import datetime
 import datetime
 import random
 import pytz
-import string
 
 class DataBuilder:
     def __init__(self):
         self.fake = Faker("id_ID")
 
-    def date_future(self):
-        return  self.fake.date_between(start_date='today', end_date='+60d').strftime('%Y-%m-%d')
-    def date_past(self):
-        return  self.fake.date_between(start_date='-60d', end_date='today').strftime('%Y-%m-%d')
+    def base_time(self, tz='Asia/Jakarta'):
+        current_time = datetime.datetime.now(pytz.timezone(tz)).strftime('%Y-%m-%dT%H:%M:%S.%f%z')
+        return current_time
 
-    def generate_random_time():
-        # Waktu saat ini
-        current_time = datetime.datetime.now()
+    def future_time(self, days=1, tz='Asia/Jakarta'):
+        current_time = datetime.datetime.now(pytz.timezone(tz))
+        future_time = current_time + datetime.timedelta(days=days)
+        future_time = future_time.strftime('%Y-%m-%dT%H:%M:%S.%f%z')
+        return future_time
 
-        # Ambil tahun, bulan, dan tanggal dari waktu saat ini
-        year = current_time.year
-        month = current_time.month
-        day = current_time.day
+    def past_time(self, days=1, tz='Asia/Jakarta'):
+        current_time = datetime.datetime.now(pytz.timezone(tz))
+        past_time = current_time - datetime.timedelta(days=days)
+        past_time = past_time.strftime('%Y-%m-%dT%H:%M:%S.%f%z')
+        return past_time
 
-        # Waktu acak pada hari yang sama dengan waktu saat ini
-        future_time = datetime.datetime(year, month, day,
-                                        hour=random.randint(current_time.hour + 1, 23),
-                                        minute=random.randint(0, 59),
-                                        second=random.randint(0, 59))
+    def time_same_day(self, tz='Asia/Jakarta'):
+        current_time = datetime.datetime.now(pytz.timezone(tz))
 
-        # Mengonversi ke zona waktu yang diinginkan (misalnya, Waktu Indonesia Barat)
-        tz = pytz.timezone('Asia/Jakarta')
-        current_time = tz.localize(current_time)
-        future_time = tz.localize(future_time)
+        future_hour = random.randint(current_time.hour + 1, 23)
+        future_minute = random.randint(0, 59)
+        future_second = random.randint(0, 59)
 
-        # Format waktu dalam string yang diinginkan
+        future_time = datetime.datetime(
+            year=current_time.year,
+            month=current_time.month,
+            day=current_time.day,
+            hour=future_hour,
+            minute=future_minute,
+            second=future_second
+        )
+
+        future_time = pytz.timezone(tz).localize(future_time)
+
         current_time_str = current_time.strftime('%Y-%m-%dT%H:%M:%S.%f%z')
         future_time_str = future_time.strftime('%Y-%m-%dT%H:%M:%S.%f%z')
 
-        return current_time_str, future_time_str
+        return {"current_time": current_time_str, "future_time": future_time_str}
 
+    def basic_sentence(self, length):
+        faker = Faker("id_ID")
+        sentence = faker.sentence()
+        words = faker.words(length)
+        sentence = ' '.join(words)
+        return sentence[:length]
 
-    def default_sentence(self):
-        return  self.fake.text(max_nb_chars=40)
+    def format_set_sentence(self, length, charset):
+        if charset == "ASCII":
+            chars = ''.join(chr(i) for i in range(32, 127))
+        elif charset == "Unicode":
+            chars = ''.join(chr(i) for i in range(0x20, 0x7F)) + ''.join(chr(i) for i in range(0x00A1, 0x00FF))
+        elif charset == "ISO8859":
+            chars = ''.join(chr(i) for i in range(0x20, 0x7F)) + ''.join(chr(i) for i in range(0xA0, 0xFF))
+        elif charset == "EBCDIC":
+            chars = ''.join(chr(i) for i in range(64, 128))
+        else:
+            raise ValueError("Invalid charset specified.")
+        
+        return ''.join(random.choice(chars) for _ in range(length))
+    
+    def count_date(self, start_at, end_at):
+        date_format = "%Y-%m-%dT%H:%M:%S.%f%z"
+        start_date = datetime.datetime.strptime(start_at, date_format).date()
+        end_date = datetime.datetime.strptime(end_at, date_format).date()
+        date_difference = end_date - start_date
+        days_difference = date_difference.days
 
-    def make_sentence(self, length, digits=False, punctuation=False, whitespace=False):
-        if length < 0:
-            return "Length must be a non-negative integer."
+        return days_difference
 
-        characters = string.ascii_letters
-        if digits:
-            characters += string.digits
-        if punctuation:
-            characters += string.punctuation
-        if whitespace:
-            characters += string.whitespace
-
-        if not characters:
-            return "No valid character set selected."
-
-        # Generate a random sentence of the specified length
-        sentence = ''.join(random.choice(characters) for _ in range(length))
-
-        return sentence
-
-
-# Sample Usage
-# past_sameday, future_sameday = generate_random_time()
-# print("past_sameday =", past_sameday)
-# print("future_sameday =", future_sameday)
+    def reformat_date(self, datetime_string):
+        try:
+            original_datetime = datetime.datetime.strptime(datetime_string, "%Y-%m-%dT%H:%M:%S.%f%z")
+        except ValueError:
+            return "Invalid datetime format. Please provide a datetime string in the format 'YYYY-MM-DDTHH:MM:SS.ssssss±HH:MM'."
+        
+        reformatted_date = original_datetime.strftime("%Y-%m-%d")
+        
+        return reformatted_date
+        
+    def get_task_by_id(self, task_list, target_id):
+        for task in task_list:
+            if task.get("id") == target_id:
+                return task
+        return None
+    
+    def count_task_status(self, task_list):
+        todo_count = sum(1 for task in task_list if task["status"] == "todo")
+        done_count = sum(1 for task in task_list if task["status"] == "done")
+        return {"todo_task_count": todo_count, "done_task_count": done_count}
